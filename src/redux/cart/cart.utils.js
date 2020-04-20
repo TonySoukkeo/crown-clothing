a623ea3f3afadfa21e62aa1a8ef0a0bdcf7546ab
Tony <tonysoukkeodesign@gmail.com>
2020-04-20 09:46:53 -0600

    created cart-tem component and connected our cart dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
-const addItemToCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToAdd.id
   );
